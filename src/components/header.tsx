5b40bbe154387760bda385ff594b66b4670ebd5e
ASleepingBazza <elias.e.fredriksson@gmail.com>
2022-05-16 11:26:05 +0200

    Implemented scss and basic structure

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+import "../scss/components/Header.scss";
+
+export default function Header() {
+    return (
+        <header>
+            <h1>HEADER</h1>
+        </header>
+    );
+}
---
+++ src/components/main.tsx
@@ -0,0 +1,9 @@
+import "../scss/components/Main.scss";
+
+export default function Main() {
+    return (
+        <main>
+            <h1>Main</h1>
+        </main>
+    );
+}
